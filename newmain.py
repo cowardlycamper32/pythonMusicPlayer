1f44e587523cb6a47446c4acd498ea425aca8530
nova <nova@cutey.com>
2025-10-09 16:42:13 +0100

    started again again

--- newmain.py
+++ newmain.py
@@ -1,61 +1,49 @@
 from time import sleep
-import keyboard as kb
 import pygame
-import pathlib
 from pygame import mixer, RLEACCEL
 from sys import argv
 import os
 import random
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
-import pygame_gui
-import pygame_gui.data
-import CustomKeys.Media as MediaKeys
-import pynput.keyboard
+from  os.path import join
 
-if os.name == 'linux':
-    TEMP_DIR = f"{pathlib.Path.home()}/.temp/"
-else:
-    TEMP_DIR = f"{pathlib.Path.home()}/.temp/"
+TEMP_DIR = "/home/nova/.temp/"
 
-if not pathlib.Path(TEMP_DIR).exists():
-    os.mkdir(TEMP_DIR)
-if not pathlib.Path(TEMP_DIR + "images").exists():
-    os.mkdir(TEMP_DIR + "images")
-
-def getExecutableDir():
-    out = ""
-    temp = __file__
-    print(temp)
-    help = temp.split("/")
-    if len(help) <= 1:
-        help = temp.split("\\")
-    help.pop(-1)
-    print(help)
-    for dir in help:
-        out += f"{dir}/"
-    print(out)
-    return out
-
-EXECUTABLE_DIR = getExecutableDir()
-pygame.init()
 pygame.display.init()
+pygame.font.init()
+
+font = pygame.font.SysFont("monospace", 24)
 display = pygame.display.set_mode((800, 800))
-guiManager = pygame_gui.UIManager((800, 800))
 
 if len(argv[1:]) < 1:
     print("Usage: python main.py <path>")
     exit(1)
+
+def getExecPath():
+    if "/" in __file__:
+        temp = __file__.split("/")
+    else:
+        temp = __file__.split("\\")
+    temp.pop(-1)
+    #print(temp)
+    out = ""
+    for i in temp:
+        out += i + "/"
+    #print(out)
+    return out
+
 def getCoverImage(path):
+    audio = MP3(path)
+    cover = audio.get("APIC:Cover")
     try:
-        audio = MP3(path)
-        cover = audio.get("APIC:Cover")
         open(TEMP_DIR + "images/cover.png", "w+b").write(cover.data)
         image, imageRect = load_image(TEMP_DIR + "images/cover.png")
-    except (KeyError, AttributeError):
-        image, imageRect = load_image(EXECUTABLE_DIR + "placeholder.png")
-    display.blit(pygame.transform.scale(image, display.get_size()), (0,0), display.get_rect())
-    #return cover
+        display.blit(pygame.transform.scale(image, display.get_size()), (0,0), display.get_rect())
+        return cover
+    except:
+        image, imageRect = load_image(getExecPath() + "placeholder.png")
+        display.blit(pygame.transform.scale(image, display.get_size()), (0,0), display.get_rect())
 
 def getArtist(path):
     try:
@@ -71,17 +59,12 @@ def getTrackName(path):
         trackName = audio.get("title")
         return trackName[0]
     except:
-        return path.split("/")[-1]
+        return path.split("/")[-1].split(".")[0]
 
 def getTrackLen(path):
-    try:
-        audio = MP3(path, ID3=EasyID3)
-        trackLen = audio.info.length
-        return trackLen*1000
-    except:
-        print(path)
-        return 0
-
+    audio = MP3(path, ID3=EasyID3)
+    trackLen = audio.info.length
+    return trackLen
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('images', name)
@@ -105,40 +88,12 @@ if argv[1] == "*" or argv[1] == "all":
 else:
     songPaths.append(argv[1])
 
-def skipNext(calledFrom="InputLoop"):
-    if calledFrom == "InputLoop" or (calledFrom == "global" and not (pygame.key.get_focused() or pygame.mouse.get_focused())):
-        global queueHeader
-        queueHeader += 1
-        return
-    if calledFrom == "global":
-        print("HELLLLLLLLLLLL")
+if "--shuffle" in argv:
+    random.shuffle(songPaths)
 
-def skipPrevious():
+def skipNext():
     global queueHeader
-    queueHeader -= 1
-
-def onKeyPress(key):
-    global isFocused
-    if key == pynput.keyboard.Key.media_play_pause:
-        myPause("global")
-    elif key == pynput.keyboard.Key.media_next:
-        skipNext("global")
-
-def myPause(calledFrom="InputLoop"):
-    if calledFrom == "global":
-        print("Pausing")
-    if calledFrom == "InputLoop" or (calledFrom == "global" and not (pygame.key.get_focused() or pygame.mouse.get_focused())):
-        global isPaused
-        if isPaused:
-            mixer.music.unpause()
-        else:
-            mixer.music.pause()
-
-    isPaused = not isPaused
-
-
-
-test_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 400), (400, 400)), text="Test", manager=guiManager)
+    queueHeader += 1
 
 mixer.init()
 queueHeader = 0
@@ -146,55 +101,54 @@ seekPointer = 0
 doQuit = False
 pygame.key.set_repeat()
 loop = True
-currentVolume = 1.0
-clock = pygame.time.Clock()
-testVAR = 0
 while not doQuit:
-    listener = pynput.keyboard.Listener(on_press=onKeyPress)
-    listener.start()
-    time_delta = clock.tick(60)/1000.0
     if queueHeader > len(songPaths) - 1:
         if loop:
             queueHeader = 0
         else:
             doQuit = True
             pygame.event.post(pygame.event.Event(pygame.QUIT))
-    elif queueHeader < 0 and not loop:
-        doQuit = True
-        pygame.event.post(pygame.event.Event(pygame.QUIT))
+    elif queueHeader < 0:
+        if loop:
+            queueHeader = len(songPaths) - 1
+        else:
+            doQuit = True
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
     else:
         #print(songPaths)
         #print(queueHeader)
         pygame.display.set_caption(f"{getTrackName(songPaths[queueHeader])} - {getArtist(songPaths[queueHeader])}")
-        mixer.music.set_volume(currentVolume)
         mixer.music.load(songPaths[queueHeader])
         mixer.music.play()
         songEnded = False
         isPaused = False
-        getCoverImage(songPaths[queueHeader])
-        pygame.display.update()
         while not songEnded or mixer.get_busy():
             seekPointerTemp = seekPointer
             if seekPointer != seekPointerTemp:
                 mixer.music.set_pos(seekPointer)
             seekPointer = mixer.music.get_pos()/1000
-            if seekPointer >= getTrackLen(songPaths[queueHeader])/1000 or mixer.music.get_pos() == -1:
+            display.fill((1, 1, 1), pygame.Rect(0, 0, 800, 800))
+            debug = font.render(str(seekPointer), False, (255, 0, 0))
+            debug2 = font.render(str(getTrackLen(songPaths[queueHeader])), False, (255, 0, 0))
+            debug3 = font.render(str(songEnded), False, (255, 0, 0))
+            getCoverImage(songPaths[queueHeader])
+            display.blit(debug, (0,0))
+            display.blit(debug2, (0,700))
+            display.blit(debug3, (700,0))
+
+            pygame.display.update()
+            if int(seekPointer) >= getTrackLen(songPaths[queueHeader]) or int(mixer.music.get_pos()) == -1:
                 songEnded = True
+                queueHeader = queueHeader + 1
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit(0)
-
-                if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                    if event.ui_element == test_button:
-                        myPause()
-
                 if event.type == pygame.KEYDOWN:
-                    print(event.key)
                     if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                         pygame.event.post(pygame.event.Event(pygame.QUIT))
 
-                    if (event.key == pygame.K_RIGHT and pygame.key.get_mods() & pygame.KMOD_CTRL) or event.key == MediaKeys.K_NEXT_TRACK or kb.is_pressed("next track"):
+                    if event.key == pygame.K_RIGHT and pygame.key.get_mods() & pygame.KMOD_CTRL:
                         try:
                             mixer.music.rewind()
                             mixer.music.set_pos(getTrackLen(songPaths[queueHeader])/1000)
@@ -205,7 +159,7 @@ while not doQuit:
                         except pygame.error:
                             pass
 
-                    if (event.key == pygame.K_LEFT and pygame.key.get_mods() & pygame.KMOD_CTRL) or event.key == MediaKeys.K_PREVIOUS_TRACK or kb.is_pressed("previous track"):
+                    if event.key == pygame.K_LEFT and pygame.key.get_mods() & pygame.KMOD_CTRL:
                         try:
                             if mixer.music.get_pos() > 5000:
                                 mixer.music.rewind()
@@ -214,23 +168,10 @@ while not doQuit:
                                 songEnded = True
                         except pygame.error:
                             pass
-                    if event.key == pygame.K_SPACE or event.key == pygame.K_p or event.key == MediaKeys.K_PAUSE_PLAY or kb.is_pressed("play/pause media"):
-                        myPause()
-
-                    if event.key == pygame.K_DOWN:
-                        mixer.music.set_volume(currentVolume-0.1)
-                        currentVolume -= 0.1
-                        if currentVolume < 0:
-                            currentVolume = 0
-                        print(currentVolume)
-                    if event.key == pygame.K_UP:
-                        mixer.music.set_volume(currentVolume+0.1)
-                        currentVolume += 0.1
-                        print(currentVolume)
-                        if currentVolume > 1:
-
-                            currentVolume = 1.0
-                guiManager.process_events(event)
-            guiManager.update(time_delta)
-            guiManager.draw_ui(display)
-            pygame.display.update()
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_p:
+                        if isPaused:
+                            mixer.music.unpause()
+                            isPaused = False
+                        else:
+                            mixer.music.pause()
+                            isPaused = True
---
+++ newnewmain.py
@@ -0,0 +1,86 @@
+import pygame
+from pygame import mixer
+from mutagen.mp3 import MP3
+import mutagen.easyid3 as easyid3
+from sys import argv
+from os import name
+from os.path import expanduser
+
+if len(argv[1:]) < 1:
+    print("""Usage: python newnewmain.py <songs> [args]
+    \"--shuffle\": shuffle the songs list
+    \"--help\": print this help and exit""")
+    exit()
+
+for arg in argv[1:]:
+    if "--help" in arg:
+        print("""Usage: python newnewmain.py <songs> [args]
+            \"--shuffle\": shuffle the songs list
+            \"--loop=[all/song]\": loop the songs list or singular song respectivly
+            \"--help\": print this help and exit""")
+        exit()
+
+if name == "linux":
+    for arg in argv:
+        if "--temp-dir" in arg:
+            temp = arg.split("=")
+            TEMP_DIR = temp[1]
+        else:
+            TEMP_DIR = "~/.temp"
+
+elif name == "nt":
+    for arg in argv:
+        if "--temp-dir" in arg:
+            temp = arg.split("=")
+            TEMP_DIR = temp[1]
+        else:
+            TEMP_DIR = expanduser("~") + "/.temp"
+
+class song():
+    def __init__(self, path):
+        self.path = path
+        self.cover = self.getCover()
+        self.title = self.getTitle()
+        self.artist = self.getArtist()
+        self.length = self.getLen()
+
+    def getCover(self):
+        """
+        Gets cover data from metadata of mp3 at APIC:Cover
+        :return: cover binary data
+        """
+        audio = MP3(self.path)
+        cover = audio.tags.get("APIC:Cover")
+        return cover.data
+
+    def getTitle(self) -> str:
+        """
+        Gets title from metadata of mp3 at id3:title
+        :return: title;
+        """
+        audio = MP3(self.path, ID3=easyid3)
+        title = audio.info.title
+        return title
+
+    def getArtist(self) -> str:
+        """
+        Gets artist from metadata of mp3 at id3:artist
+        :return: first artist in artists;
+        """
+        audio = MP3(self.path, ID3=easyid3)
+        artist = audio.info.artist[0]
+        return artist
+    def getLen(self) -> float:
+        """
+        Gets length from metadata of mp3 at id3:length in SECONDS
+        :return: length of file from metadata;
+        """
+        audio = MP3(self.path)
+        length = audio.info.length()
+        return length
+
+class manager:
+    def __init__(self, width: int = 800, height: int = 800):
+        pygame.display.init()
+        display = pygame.display.set_mode((width, height))
+
